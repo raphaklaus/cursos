564248cd742092b2fb9c86c4d86e7a6e102dbcdb
Raphael D. Pinheiro <raphaklaus@gmail.com>
2022-05-01 21:27:56 +0200

    Aulas 01 e 02 de JS

---
+++ 01.js
@@ -0,0 +1,10 @@
+// Instalar NodeJS, acessar: https://nodejs.org/ e clicar para baixar a versão "LTS"
+// Instalar o editor de texto Visual Studio Code: https://code.visualstudio.com/
+
+var nomeProduto = 'Leite';
+var precoProduto = 1.99;
+var quantidadeProduto = 5;
+
+console.log(nomeProduto);
+console.log(precoProduto);
+console.log(quantidadeProduto);
---
+++ 02.js
@@ -0,0 +1,12 @@
+var nomeProduto = 'Leite';
+var precoProduto = 1.99;
+var quantidadeProduto = 0;
+
+if (quantidadeProduto == 0) {
+  console.log('Produto indisponível!');
+} else {
+  console.log('Produto disponível em estoque');
+}
+
+// if = se
+// else = caso contrário
